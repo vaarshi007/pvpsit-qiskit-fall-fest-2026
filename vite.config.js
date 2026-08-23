413e167eb5a9eba76b54718b5f747a8f7d6dd86f
Joshua Daniel <pjoshuadaniel387@gmail.com>
2026-08-24 00:26:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/pvpsit-qiskit-fall-fest-2026/',
 })
